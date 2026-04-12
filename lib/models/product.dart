ac488aadf615bdfde7c7673a2b70c65eccd804b5
CloudElixir <cybe4info@gmail.com>
2026-04-12 19:57:16 +0530

    Remove duplicate Flutter files from root - keep only in girgo_flutter directory

--- lib/models/product.dart
+++
@@ -1,2 +0,0 @@
-export '../constants/products.dart' show Product;
-
--- lib/utils/payment_platform.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/foundation.dart' show kIsWeb, defaultTargetPlatform;
-import 'package:flutter/material.dart' show TargetPlatform;
-
-/// Razorpay native SDK runs on Android and iOS only (not web/desktop).
-bool get isRazorpayCheckoutSupported =>
-    !kIsWeb &&
-    (defaultTargetPlatform == TargetPlatform.android ||
-        defaultTargetPlatform == TargetPlatform.iOS);
