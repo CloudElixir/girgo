e73fade5ad20647c8cea06454a8d19260d3797dd
CloudElixir <cybe4info@gmail.com>
2026-04-14 14:50:42 +0000

    fix shared_preferences build error

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/foundation.dart' show kIsWeb, kReleaseMode;
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
 import 'services/firebase_service.dart';
 import 'providers/auth_provider.dart';
